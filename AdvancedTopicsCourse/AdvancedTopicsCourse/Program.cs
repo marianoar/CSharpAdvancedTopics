5b0b15452bd3a7fc311a64a55b2d10e109909297
Mariano Luis Arias <mariano.arias@neoris.com>
2025-03-05 20:45:51 -0300

    Generics

---
+++ AdvancedTopicsCourse/AdvancedTopicsCourse/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Generics;
+using System.Reflection;
+
+Console.WriteLine("Hello, World!");
+
+Book book = new Book { Isbn = "123", Title = "C# Sarasa" };
+
+var numbers = new List<int>();
+numbers.Add(1);
+
+var books = new BookList();
+books.Equals(book);
+
+var numbersGen = new GenericList<int>();
+numbers.Add(1);
+
+var booksGen = new GenericList<Book>();
+booksGen.Add(book);
+
+var dictionay = new GenericDictionary<string, Book>();
+dictionay.Add("222", new Book());
+
+var number = new Generics.Nullable<int>(5);
+Console.WriteLine("Has value ?" + number.HasValue);
+Console.WriteLine("VAlue: "+number.GetValueOrDefault());
+
+
---
+++ AdvancedTopicsCourse/Generics/Utilities.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    //where T: IComparable
+    //where T: Product
+    //where T: struct
+    //where T: class
+    //where T: new ()
+    public class Utilities<T> where T: new() // <T> where T: IComparable
+    {
+        public int Max(int a, int b)
+        {
+            return a > b ? a : b;
+        }
+
+        //generics version
+        public T Max<T>(T a, T b) where T: IComparable // esto puede ir a nivel de clase
+        {
+            return a.CompareTo(b) > 0 ? a : b;
+        }
+
+        public void DoSomething(T value)
+        {
+            var obj = new T();
+        }
+    }
+}
