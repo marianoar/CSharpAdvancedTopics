895395220d07fbc2e9da2835b97391d025747487
Mariano Arias <arias.mariano.ar@gmail.com>
2025-03-13 11:39:07 -0300

    dynamics

---
+++ AdvancedTopicsCourse/Dynamics/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello Dynamics!");
+
+dynamic hola = "Algo";
+
+hola = 10;
+
+dynamic a = 5;
+dynamic b = 10;
+var c = a + b;
+
+int d = 4;
+dynamic f = d;
+long l = d;
